81fd41a27e5c3ef444ad2c495d5682345203f604
guburchardt <guburchardt@hotmail.com>
2026-02-09 15:15:00 -0300

    feat: create application layer and appointment logic

---
+++ barbershop/Application/Abstractions/Persistence/IAppointmentRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using barbershop.Domain.Entities;
+
+namespace barbershop.Application.Abstractions.Persistence;
+
+public interface IAppointmentRepository
+{
+    // There is conflict?
+    Task<bool> HasOverlapAsync(Guid employeeId, DateTime startAt, DateTime endAt, CancellationToken ct);
+    Task AddAsync (Appointment appointment, CancellationToken ct);
+}
