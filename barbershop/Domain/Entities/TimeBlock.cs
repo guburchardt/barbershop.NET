1ffdc5e23434f59e4aca0c541cfe816ae9caffc9
Gustavo Burchardt <107526827+guburchardt@users.noreply.github.com>
2026-02-09 14:00:20 -0300

    feat: create TimeBlock entity (#4)

---
+++ barbershop/Domain/Entities/TimeBlock.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace barbershop.Domain.Entities;
+
+public class TimeBlock
+{
+    public Guid Id { get; private set; }
+    // if null, block all employees
+    public Guid? EmployeeId { get; private set; }
+    public DateTime StartAt { get; private set; }
+    public DateTime EndAt { get; private set; }
+    public string Reason { get; private set; } = null!;
+    public DateTime CreatedAt { get; private set; }
+
+    //Future ORM
+    protected TimeBlock() {}
+
+    public TimeBlock(DateTime startAt, DateTime endAt, string reason, Guid? employeeId = null)
+    {
+        if (endAt <= startAt)
+            throw new ArgumentException("EndAt must be grater than StartAt.");
+
+        if (string.IsNullOrWhiteSpace(reason))
+            throw new ArgumentException("Reason is required.");
+
+        Id = Guid.NewGuid();
+        EmployeeId = employeeId;
+        StartAt = startAt;
+        EndAt = endAt;
+        Reason = reason;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
