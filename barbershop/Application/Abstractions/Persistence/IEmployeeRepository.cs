8577e45a00baf4198b50fdd3be671a6db555f1a5
guburchardt <guburchardt@hotmail.com>
2026-02-14 19:26:09 -0300

    feat: create abstractions interface

--- barbershop/Application/Abstractions/Persistence/IEmployeeRepository.cs
+++ barbershop/Application/Abstractions/Persistence/IEmployeeRepository.cs
@@ -7,6 +7,6 @@ public interface IEmployeeRepository
 {
     Task AddAsync (Employee employee, CancellationToken ct);
     Task <Employee?> GetByIdAsync (Guid id, CancellationToken ct);
-    Task <IReadOnlyList<Client>> GetAllAsync (CancellationToken ct);
-    Task UpdateAsync (Client client, CancellationToken ct);
+    Task <IReadOnlyList<Employee>> GetAllAsync (CancellationToken ct);
+    Task UpdateAsync (Employee employee, CancellationToken ct);
 }
